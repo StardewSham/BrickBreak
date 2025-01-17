26f419481d33a1210408768fbdd7c7b21b95c993
StardewSham <stardewsham@gmail.com>
2025-01-17 03:50:53 +0000

    commit -m “submission 1/16”

--- workspace/Ball.java
+++ workspace/Ball.java
@@ -1,13 +1,83 @@
+/*
+*Melike Yildirim
+*1.2.2025
+*i'm actually submitting this on the 16th cause i want to come in tomorrow and tell you i submitted all my assignments
+*while also keeping my promise of turning this in before the test
+*i also printed out every single one of the tests
+*i'm pulling an all nighter for java cause i need to score at least a 90% on the midterm
+*for my very little pride and sanity that is left
+*also for my weak grade
+*/
+
+
 import java.awt.*;
 
+//need to add pre and post conditions
+
+//x,y, size
+
 public class Ball {
-	//your code here!
-	
+	//your code here
+
 	//don't forget you need instance variables:
-	
+	private int size;
+	private int x;
+	private int y;
+	private int xVelocity = 1;
+	private int yVelocity = 1;
 	
 	//constructor(s):
-	
+	public Ball(int x, int y, int size){
+		this.x = x;
+		this.y =y;
+		this.size=size;
+	}
 	//methods:
 
+	//getters:
+	public int getXpos(){
+		return x;
+	}
+	public int getYpos(){
+		return y;
+	}
+	public int getSize(){
+		return size;
+	}
+	public int getXVelocity(){
+		return xVelocity;
+	}
+	public int getYVelocity(){
+		return yVelocity;
+	}
+
+	//setters
+	public void setX(int x){
+		this.x = x;
+	}
+	public void setY(int y){
+		this.y = y;
+	}
+	public void setXVelocity(int xVelo){
+		xVelo = xVelocity;
+	}
+	public void setYVelocity(int yVelo){
+		yVelo = yVelocity;
+	}
+
+	public void reverseX(){
+		xVelocity*=-1;
+	}
+	public void reverseY(){
+		yVelocity*=-1;
+	}
+	public void move(){
+		x += xVelocity;
+		y += yVelocity;
+	}
+
+	public void draw (Graphics g){
+		g.setColor(Color.BLUE);
+		g.fillOval(x, y, size, size);
+	}
 }
--- workspace/Paddle.java
+++ workspace/Paddle.java
@@ -1,13 +1,74 @@
 import java.awt.Graphics;
 import java.awt.*;
+/*
+*Melike Yildirim
+*1.2.2025
+*This sets up the paddle
+*i didn't think the project would be this easy wow i really need to do my assignments more often
+*i think the most annoying thing about this was either the pre and post conditions, reading through your code, or the velocity, although i think i like the latter way more. it's not as boring blah blah blah
+*complaint complaint complaint complaint complaint complaint complaint
+*/
+
+//need to add pre and post conditions
+
+//velocity and all that jazz
+
 
 public class Paddle {
 
 	//your code here!
-	
+	private int x;
+	private int y; 
+	private int width;
+	private int height;
+	private int velocity;
+
+	public Paddle(int x, int y, int width, int height){
+		this.x = x;
+		this.y = y;
+		this.width=width;
+		this.height=height;
+		this.velocity=0;
+	}
 	//don't forget you need instance variables:
 	
-	
+	public int getX(){
+		return x;
+	}
+	public int getY(){
+		return y;
+	}
+	public int getHeight(){
+		return height;
+	}
+	public int getWidth(){
+		return width;
+	}
+	public int getVelocity() {
+        return velocity;
+    }
+	public void setX(int x) {
+        this.x = x;
+    }
+	public void setVelocity(int velocity) {
+        this.velocity = velocity;
+    }
+	public void addVelocity(int velocity){
+		this.velocity+=velocity;
+	}
+	public void move(){
+		x+=velocity;
+		if (x<0){
+			x=0;
+		}
+		if (x+width>682){
+			x=682-width;
+		}
+	}
+	public void draw(Graphics g){
+		g.setColor(Color.RED);
+		g.fillRect(x,y,width,height);
+	}
 	//constructor(s):
 	
 	//methods:
